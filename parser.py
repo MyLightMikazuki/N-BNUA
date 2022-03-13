932db86b9368c4eaca59d7bca344f81f2f4989fd
MyLightMikazuki <mylightmikazuki@gmail.com>
2022-03-13 18:26:36 +0800

    Fix bad SSL on MacOs.

--- parser.py
+++ parser.py
@@ -1,7 +1,10 @@
-import argparse
+import argparse, ssl, platform
 from yt_process import YtProcess
 
 if __name__ == "__main__":
+    if platform.system() == 'darwin':
+        ssl._create_default_https_context = ssl._create_unverified_context
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--saved-directory", metavar="<string>",
                         help="The json file name.", default='saved' ,type=str)
@@ -10,5 +13,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ytp = YtProcess(saved_directory=args.saved_directory, tasks_directory=args.tasks_directory)
-
-
